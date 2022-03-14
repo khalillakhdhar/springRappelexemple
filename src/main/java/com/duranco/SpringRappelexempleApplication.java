3d1a2962b238f7402119185991869d56eb79209a
khalil <khalillakhdharatc@gmail.com>
2022-03-14 09:50:53 +0100

    Initial commit

---
+++ src/main/java/com/duranco/SpringRappelexempleApplication.java
@@ -0,0 +1,13 @@
+package com.duranco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRappelexempleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRappelexempleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/duranco/SpringRappelexempleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duranco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRappelexempleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
